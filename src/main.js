928e13e8dad1a776ec1404a688df9d057fdd2d18
Juanmaes83 <juanmaes83@gmail.com>
2026-08-14 18:58:54 +0200

    feat(v2.3): boot stabilized runtime

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './style.css';
 import './v21.css';
 import './v22.css';
-import {bootV22} from './v22-runtime.js';
-bootV22();
-const duration=document.querySelector('#job-duration');
-if(duration)duration.max='14';
+import './v23.css';
+import {bootV23} from './v23-runtime.js';
+bootV23();
