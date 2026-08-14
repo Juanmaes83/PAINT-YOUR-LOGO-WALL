13fbeb32c58fe5313b5884ee5db028a83e0a8d64
Juanmaes83 <juanmaes83@gmail.com>
2026-08-14 19:15:28 +0200

    fix(v2.3): wire onboarding text action to native text controller

--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ import './v22.css';
 import './v23.css';
 import {bootV23} from './v23-runtime.js';
 bootV23();
+const heroText=document.querySelector('#add-text-job-hero');
+const panelText=document.querySelector('#add-text-job');
+if(heroText&&panelText)heroText.addEventListener('click',()=>panelText.click());
