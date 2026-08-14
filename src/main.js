6ab01f4e8183e41817b83546a044ee80aa06c7b3
Juanmaes83 <juanmaes83@gmail.com>
2026-08-14 19:15:16 +0200

    fix(v2.3): remove duplicate-id workaround

--- src/main.js
+++ src/main.js
@@ -4,4 +4,3 @@ import './v22.css';
 import './v23.css';
 import {bootV23} from './v23-runtime.js';
 bootV23();
-queueMicrotask(()=>{const buttons=[...document.querySelectorAll('#add-text-job')];if(buttons.length>1)buttons.slice(1).forEach(button=>button.addEventListener('click',()=>buttons[0].click()))});
